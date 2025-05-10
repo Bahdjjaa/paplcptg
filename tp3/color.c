6e6a4cb672ae21c5fd9df0af16987b440ea880d4
bluezenith <mail@mail.mail>
2025-05-10 02:19:09 +0200

    base

--- tp3/color.c
+++ tp3/color.c
@@ -10,7 +10,6 @@ enum {
 
 int adj[N][N];	/* matrice d'adjacences */
 int deg[N], coul1[N], coul2[N], coul3[N];
-int trouv = 0;	/* stoppe algo exact quand coloration trouvee */
 
 void
 gengraph(int n, int p)
@@ -60,6 +59,7 @@ convient(int *coul, int x, int c)
 	return 1;
 }
 
+/* coloration gloutonne */
 int
 colorsom(int *coul, int x)
 {
@@ -71,48 +71,39 @@ colorsom(int *coul, int x)
 	return c;
 }
 
+/* k-coloration, backtracking */
 void
-colorrr(int x, int k)
+colorback(int *coul, int x, int k, int *stop)
 {
-	int i, c;
+	int c;
 
-	if(x == N){
-		printf("Coloration en %d couleurs trouvée.\n", k);
-		for(i = 0; i < N; i++)
-			printf("couleur de %d : %d\n", i, coul1[i]);
-		trouv = 1;
-	}else{
+	if(x == N)
+		*stop = 1;
+	else{
 		for(c = 1; c <= k; c++){
-			if(convient(coul1, x, c)){
-				coul1[x] = c;
-				colorrr(x+1, k);
-				if(trouv)
+			if(convient(coul, x, c)){
+				//printf("coul som %d %d\n", x, c);
+				coul[x] = c;
+				colorback(coul, x+1, k, stop);
+				if(*stop)
 					return;
 			}
 		}
 	}
 }
 
-/* teste si un graphe admet k-coloration */
-void
-colorexact(int k)
-{
-	memset(coul1, 0, N * sizeof(int));
-	colorrr(0, k);
-	if(!trouv)
-		printf("Pas de coloration en %d couleurs\n", k);
-}
-
 int
-nbchroma()
+colexact(int *coul)
 {
-	int i;
+	int k, stop;
 
-	/* variation de k */
-	for(i = 0; i < N && !trouv; i++){
-		colorexact(i);
+	for(k = stop = 0; k < N && stop == 0; k++){
+		memset(coul, 0, N * sizeof(int));
+		colorback(coul, 0, k, &stop);
+		if(stop == 0)
+			printf("Pas de coloration en %d couleurs\n", k);
 	}
-	return i-1;
+	return k-1;	/* k-colo, nb chromatique */
 }
 
 int
@@ -126,6 +117,18 @@ colorglouton(int *coul)
 		if((c = colorsom(coul, i)) > nc)
 			nc = c;
 	return nc;
+}
+
+/* affiche les etapes du dsatur */
+void
+prttabdsat(int *dsat, int s, int c)
+{
+	int i;
+
+	printf("c(%d) = %d\t", s, c);
+	for(i = 0; i < N; i++)
+		printf("%d\t", dsat[i]);
+	printf("\n");
 }
 
 int
@@ -157,7 +160,7 @@ actuvois(int *dsat, int *coul)
 int
 dsatur(int *coul)
 {
-	int i, s, c, nc, soc, dsat[N];
+	int s, c, nc, soc, dsat[N];
 
 	memset(coul, 0, N * sizeof(int));
 	memmove(dsat, deg, N * sizeof(int));
@@ -167,10 +170,7 @@ dsatur(int *coul)
 			nc = c;
 		dsat[s] = -1;
 		actuvois(dsat, coul);
-		printf("c(%d) = %d\t", s, c);
-		for(i = 0; i < N; i++)
-			printf("%d\t", dsat[i]);
-		printf("\n");
+		prttabdsat(dsat, s, c);
 	}
 	return nc;
 }
@@ -184,7 +184,9 @@ main()
 	gengraph(N, 50);
 	initdeg();
 	affgraph(N);
-	printf("nbchroma : %d\n", nbchroma());
+	printf("nbchroma : %d\n", colexact(coul1));
+	for(i = 0; i < N; i++)
+		printf("couleur de %d : %d\n", i, coul1[i]);
 	colorglouton(coul2);
 	for(i = 0; i < N; i++)
 		printf("(glout) col %d : %d deg %d\n", i, coul2[i], deg[i]);
